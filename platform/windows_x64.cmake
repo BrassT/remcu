7c2045f0850ab01bd26a195aed49fe72fc9b6dba
fatelike <53198132+BrassT@users.noreply.github.com>
2023-08-09 10:25:41 +0300

    Update windows_x64.cmake

--- platform/windows_x64.cmake
+++ platform/windows_x64.cmake
@@ -2,8 +2,8 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/vars.cmake)
 
-set(CMAKE_CXX_COMPILER clang-cl)
-set(CMAKE_C_COMPILER clang-cl)
+set(CMAKE_CXX_COMPILER clang)
+set(CMAKE_C_COMPILER clang)
 
 set(MCU_INCLUDE_MK_FILE ${REMCU_VM_PATH}/mcu_utils/windows.mk)
 
